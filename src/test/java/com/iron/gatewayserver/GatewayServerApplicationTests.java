0c438216d87015f82b64be4958b152b4926283b6
withbyeongk <withbyeongk@gmail.com>
2023-12-20 01:09:49 +0900

    init : gateway-server 세팅
    아직 path 설정은 되지 않음

---
+++ src/test/java/com/iron/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iron.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
