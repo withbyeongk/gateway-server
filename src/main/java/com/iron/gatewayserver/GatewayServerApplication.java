ba50a933dd40b175374da6d3783544dcef261ea3
withbyeongk <withbyeongk@gmail.com>
2023-12-23 12:53:04 +0900

    init :
    1. gateway 라이브러리 변경('spring.cloud.gateway-mvc' -> 'spring.cloud.gateway')
    2. application class에 @EnableDiscoveryClient 어노테이션 추가
    3. 테스트용 서버페이지 추가

--- src/main/java/com/iron/gatewayserver/GatewayServerApplication.java
+++ src/main/java/com/iron/gatewayserver/GatewayServerApplication.java
@@ -2,12 +2,22 @@ package com.iron.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "this is  gateway-server";
 	}
 
 }
